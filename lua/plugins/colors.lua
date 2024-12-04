8280917cb6bc2b9dec1ff610ed86a963f3e10ebc
Galix <galu0603@gmail.com>
2024-12-04 09:22:32 +0100

    Kinda refactor

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -3,6 +3,8 @@ return {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
-    opts = {},
+    config = function()
+      vim.cmd([[colorscheme tokyonight-storm]])
+    end,
   }
 }
